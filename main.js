e4bd6c15b89992e9b5ac87556bd16fa435a83fd8
Lucas Roteño <lucasrot1@hotmail.com>
2024-05-27 01:22:18 -0300

    entrega 2

--- main.js
+++ main.js
@@ -1,57 +1,41 @@
-const rutinaRealizada = [];
+const rutinaRealizada = JSON.parse(localStorage.getItem('rutinaRealizada')) || [];
 
-function rutina(calentamiento, fuerza, flexibilidad) {
-  const ejercicios = {
-    calentamiento: calentamiento,
-    fuerza: fuerza,
-    flexibilidad: flexibilidad,
-  };
-  rutinaRealizada.push(ejercicios);
-}
+const rutina = () => {
+    const ejercicios = {
+        calentamiento: document.getElementById("seriesCalentamiento").value,
+        fuerza: document.getElementById("seriesFuerza").value,
+        flexibilidad: document.getElementById("seriesFlexibilidad").value,
+    };
 
-function verRutina() {
-    for (const ejercicios of rutinaRealizada) {
-      alert(
-        "Tu rutina de calentamiento es: " +
-          ejercicios.calentamiento +
-          " minutos, tu rutina de fuerza es " +
-          ejercicios.fuerza +
-          " series, y tu rutina de flexibilidad es " +
-          ejercicios.flexibilidad +
-          " minutos."
-      );
+    if (ejercicios.calentamiento && ejercicios.fuerza && ejercicios.flexibilidad) {
+        rutinaRealizada.push(ejercicios);
+        localStorage.setItem('rutinaRealizada', JSON.stringify(rutinaRealizada));
+    } else {
+        alert('Por favor, selecciona las opciones.');
     }
-  }
+};
 
+const verRutina = () => {
+  const rutinaLista = document.getElementById('rutinaLista');
+  rutinaLista.innerHTML = '';
+  rutinaRealizada.forEach((ejercicios, e) => {
+      const li = document.createElement('li');
+      li.innerText = `Rutina ${e + 1}: Calentamiento: ${ejercicios.calentamiento}, Fuerza: ${ejercicios.fuerza}, Flexibilidad: ${ejercicios.flexibilidad}.`;
+      const removerButton = document.createElement('button'); 
+      removerButton.innerText = 'Remover';
+      removerButton.className = 'botonCancelar';
+      removerButton.onclick = () => removerRutina(e);
+      li.append(removerButton); 
+      rutinaLista.append(li);
+  });
+};
 
+const removerRutina = (e) => {
+    rutinaRealizada.splice(e, 1);
+    localStorage.setItem('rutinaRealizada', JSON.stringify(rutinaRealizada));
+    verRutina();
+};
 
-  function empezarRutina() {
-    let empezarRutina = confirm("¿Quieres realizar una rutina de entrenamiento?");
-    while (empezarRutina) {
-      const empezar = prompt("Ingresar acción (crear, ver series)");
-      if (empezar === "crear") {
-        const calentamiento = prompt("¿Cuántos minutos de calentamiento quieres hacer?");
-        if (calentamiento > 20) {
-          alert("No se puede realizar una rutina de calentamiento de más de 20 minutos");
-        }
-        const fuerza = prompt("¿Cuántas series de fuerza quieres hacer?");
-        if (fuerza > 7) {
-          alert("No se puede realizar una rutina de fuerza de más de 7 series");
-        }
-        const flexibilidad = prompt("¿Cuántos minutos de flexibilidad quieres hacer?");
-        if (flexibilidad > 20) {
-          alert("No se puede realizar una rutina de flexibilidad de más de 20 minutos");
-        }
-        rutina(calentamiento, fuerza, flexibilidad);
-      } else if (empezar === "ver series") {
-        verRutina();
-      } else {
-        alert("Acción no válida");
-      }
-      empezarRutina = confirm("¿Quieres realizar otra rutina o ver las series de entrenamiento?");
-    }
-    alert("¡Hasta la próxima!");
-  }
-  
-empezarRutina();
+document.getElementById('crearButton').addEventListener('click', rutina);
+document.getElementById('rutinaButton').addEventListener('click', verRutina);
 
